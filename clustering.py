63d44301bb6fe37f243f9f76fef616108b4088c3
Nikolay Pashov <nikifaets11@gmail.com>
2019-03-12 14:23:44 +0200

    fixing spaghetti codE

--- clustering.py
+++ clustering.py
@@ -6,12 +6,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()  # for plot styling
 from sklearn.datasets.samples_generator import make_blobs
 from scipy.stats import gaussian_kde
-import extract_by_density as extr
 
 
 compressed = np.load('compressed12.npy')
 
-labels_pv = np.load("data/PV/labels.npy")[:len(compressed)]
+labels_truth = np.load("data/PV/labels.npy")[:len(compressed)]
 
 print(compressed.shape)
 compressed = np.reshape(compressed, (compressed.shape[0], compressed.shape[-1]*compressed.shape[1]*compressed.shape[2]))
@@ -33,31 +32,31 @@ print("INPUT TYPE AND SHAPE", type(compressed), compressed.shape)
 
 plt.figure(1)
 
-print(labels_pv)
-print("SUM", np.sum(labels_pv))
+print(labels_truth)
+print("SUM", np.sum(labels_truth))
 
 counter=0
 
-color = np.empty((len(labels_pv)), np.float32)
-for i in range (0, len(labels_pv)):
-	if labels_pv[i]==1:
+color = np.empty((len(labels_truth)), np.float32)
+for i in range (0, len(labels_truth)):
+	if labels_truth[i]==1:
 		counter+=1
 		color[i] = 0.2
 	else:
 		color[i] = 0.8
 
 print(counter)
-labels_pv+=1
-labels_pv = labels_pv*100
+labels_truth+=1
+labels_truth = labels_truth*100
 
-print(labels_pv.shape)
+print(labels_truth.shape)
 print(color.shape)
 plt.scatter(compressed[:,0], compressed[:,1], c=color)
 plt.plot()
 
 
 swapped_compressed = np.swapaxes(compressed, 1, 0)
-plt.figure(2)
+#plt.figure(2)
 # stack cells and non-cells
 z = gaussian_kde(swapped_compressed)(swapped_compressed)
 z_sorted = np.copy(z)
@@ -65,8 +64,8 @@ idx = z_sorted.argsort()
 x, y, z_sorted = compressed[idx][0], compressed[idx][1], z_sorted[idx]
 swapped_compressed = np.swapaxes(swapped_compressed, 0, 1)
 print('SHAPE', swapped_compressed.shape)
-plt.scatter(swapped_compressed[:,0], swapped_compressed[:,1], cmap='viridis', c = z_sorted, zorder=1)
-plt.plot()
+#plt.scatter(swapped_compressed[:,0], swapped_compressed[:,1], cmap='viridis', c = z_sorted, zorder=1)
+#plt.plot()
 '''
 plt.figure(3)
 
@@ -77,34 +76,31 @@ plt.plot()
 
 plt.show()'''
 plt.show()
-
 np.save("z.npy", z)
 
-counter = 0
-counterTrue=0	
-counterFalse=0
+truePositive=0
 falsePositive=0
+trueNegative=0
 falseNegative=0
 
-#labels 1 = negative
-#labels_pos 1 = positive
-'''
-for i in range(len(labels)):
-	if labels[i] == 1:
-		counter+=1
-	if labels[i]==0 and labels_pos[i]==1:
-		counterTrue+=1
-	if labels[i]==1 and labels_pos[i]==0:
-		counterFalse+=1
-	if labels[i]==0 and labels_pos[i]==0:
-		falsePositive+=1
-	if labels[i]==1 and labels_pos[i]==1:
+for i in range(0, len(labels_truth)):
+
+	gt = labels_truth[i]
+	km = labels[i]
+
+	if gt == 1 and km ==1:
+		truePositive+=1
+
+	if gt ==1 and km == 0:
 		falseNegative+=1
-print("COUNTER", counter, counterTrue)
-print("truePositive", counterTrue)
-print("trueNegative", counterFalse)
-print("falsePositive", falsePositive)
-print("falseNegative", falseNegative)
-cluster1 = np.argmin(labels)
-cluster2 = np.argmax(labels)
-'''
+
+	if gt == 0 and km == 0:
+		trueNegative+=1
+
+	if gt == 0 and km == 1:
+		falsePositive+=1
+
+print("True Positive", truePositive)
+print("False Positive", falsePositive)
+print("True Negative", trueNegative)
+print("False Negative", falseNegative)
--- visualize.py
+++ visualize.py
@@ -30,37 +30,5 @@ def visualize_ae(num, samples, model, data):
 	res*=255
 	data+=0.5
 	data*=255
-	print("range test_data", np.min(test_data), np.max(test_data))
-	print("range data", np.min(data), np.max(data))
-	print("range res", np.min(res), np.max(res))
-	#print(np.max(res), "MEAN", np.mean(res))
-	test_imgs = list()
-	gt_imgs = list()
-	for i in range(samples):
-		print('i', i)
-		cell1,cell2 = get_samples(i,res)
-		gt1, gt2 = get_samples(num+i, data)
-		gt1 = cv2.resize(gt1,None, fx=4, fy=4, interpolation = cv2.INTER_LINEAR)
-		gt2 = cv2.resize(gt2,None,fx=4, fy=4, interpolation = cv2.INTER_LINEAR)
-		cell1 = cv2.resize(cell1,None,fx=4, fy=4, interpolation = cv2.INTER_LINEAR)
-		cell2 = cv2.resize(cell2,None,fx=4, fy=4, interpolation = cv2.INTER_LINEAR)
-		test_imgs.append((cell1, cell2))
-		gt_imgs.append((gt1,gt2))
-
-	print(len(gt_imgs), len(test_imgs))
-
-	counter = 0
-	for i in test_imgs:
-		cell1,cell2 = i
-		cv2.imshow("nucleus1_"+str(counter), cell1)
-		cv2.imshow("nucleus2_"+str(counter), cell2)
-		counter+=1
-
-	counter = 0
-	for j in gt_imgs:
-		gt1,gt2 = j
-		cv2.imshow("ground_truth1_"+str(counter), gt1)
-		cv2.imshow("ground_truth2_"+str(counter), gt2)
-		counter+=1
-		
-	cv2.waitKey()
+	
+	
